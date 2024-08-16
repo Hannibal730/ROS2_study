f9bbb36deacae16cb947d1d8f69c27020a980521
Hannibal <165776550+Hannibal730@users.noreply.github.com>
2024-08-17 05:37:59 +0900

    8/17 토픽을 구독하는 subscriber 클래스 만들기

--- my_subscriber.py
+++ my_subscriber.py
@@ -1,13 +1,13 @@
 import rclpy as rp
 from rclpy.node import Node  # ROS2에서 노드를 정의할 때 자주 사용. 원하는 클래스가 이 노드를 상속받아 사용함.
-from turtlesim.msg import Pose  # 터틀심 패키지의 Pose라는 메시지 타입 가져오기. (선형속도, 각속도 포함)
+from turtlesim.msg import Pose  # 터틀심 패키지의 Pose라는 메시지 타입 가져오기.
 
 class Turtlesimsubscriber(Node):  # 'Node' 클래스를 상속받아 'Turtlesimsubscriber'라는 클래스를 정의
     def __init__(self):  # 생성자 메서드로써
         super().__init__('turtlesim_subscriber')  # 부모 클래스(Node)의 생성자를 호출, Node클래스의 생성자의 첫 변수는 노드의 이름을 받는다. Node클래스의 인스턴스는 노드의 참조변수이고, 생성자의 변수로 받아진 이름이 노드의 이름이다. 이때 노드 이름을 'turtlesim_subscriber'로 설정
         self.subscription = self.create_subscription(
             Pose,  # 구독할 토픽의 메시지의 데이터타입
-            '/turtle1/pose',  # 구독할 토픽 이름
+            '/turtle1/pose',  # 구독할 토픽 이름. 참고로 해당 토픽은 거북이의 x,y축 위치 좌표를 발행하는 토픽이다.
             self.callback,  # 메시지를 받았을 때 호출될 콜백 함수
             10)  # 큐 사이즈
         self.subscription  # 만약 self.subscription이라는 변수가 전체 코드 실행시 사용되지 않을 경우에는 코드 편집기가 "사용되지 않은 변수 있음"이라며 경고를 보낸다. 이 경고를 방지하고자 그냥 쓴 거임.
